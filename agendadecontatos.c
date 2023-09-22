63a7f3a2b4fa151d8b5c1ff35af1091391e80b15
Clara Meira <claraa.meiraa@hotmail.com>
2023-09-22 20:16:38 -0300

    retirada da função concatenação, que estava sendo usada para melhor compreensão do trabalho do Hash.

--- agendadecontatos.c
+++ agendadecontatos.c
@@ -18,16 +18,6 @@ struct contato *criaContato(char *nome, char *tel, char *email){
     return novo; //A função retorna o ponteiro novo, que aponta para a nova estrutura de contato criada. Isso permite que o novo contato seja acessado e gerenciado em outras partes do programa.
 }
 
-//Função que calcula um valor hash a partir de uma string (para indexação na tabela hash)
-int concatenacao(char *c){
-    int key = 0, i = 0; //Duas variáveis são inicializadas: key é a variável que armazenará o valor da chave, inicializada com zero, e i é um contador inicializado com zero para percorrer os caracteres da string.
-    while(c[i] != '\0'){ //Este é um loop while que executa enquanto o caractere atual da string c não for o caractere nulo ('\0'), que indica o final da string.
-        key += c[i]; //Dentro do loop, a cada iteração, o valor numérico do caractere atual da string c (representado como um número inteiro) é adicionado à variável key. Isso acumula os valores numéricos de todos os caracteres na variável key, efetivamente concatenando os valores.
-        i++; //O contador i é incrementado para passar para o próximo caractere na string.
-    }
-    return key; // Após o loop, a função retorna o valor calculado em key, que é a chave calculada a partir da string passada como argumento.
-}
-
 //Função que calcula o índice na tabela hash usando divisão
 int funHashDiv(int key){
     return key % size; //Dentro da função, o operador % é usado para calcular o resto da divisão de key por size. Isso significa que a função retorna o valor resultante da operação de módulo.
@@ -38,8 +28,7 @@ typedef struct contato *agendaDeContatos[size];
 
 //Função para inserir um contato na tabela hash
 int inserir(agendaDeContatos Hash, struct contato *c){
-    int key = concatenacao(c->nome);  //Calcula uma chave (hash) usando o campo nome do contato c. Isso é feito chamando a função concatenacao, que soma os valores ASCII dos caracteres na string nome para gerar uma chave.
-    key = funHashDiv(key); //Calcula o índice na tabela hash com base na chave calculada anteriormente usando a função funHashDiv. Essa função aplica a operação de módulo (%) para garantir que o índice esteja dentro dos limites da tabela hash.
+    int key = funHashDiv(key); //Calcula o índice na tabela hash com base na chave calculada anteriormente usando a função funHashDiv. Essa função aplica a operação de módulo (%) para garantir que o índice esteja dentro dos limites da tabela hash.
 
     int key_inicio = key; //Verifica se a função está entrando em um loop infinito ao procurar um espaço vazio na tabela.
 
@@ -64,8 +53,7 @@ int inserir(agendaDeContatos Hash, struct contato *c){
 
 //Função para buscar um contato na tabela hash pelo nome
 struct contato *buscarContato(agendaDeContatos Hash, char *nome){
-    int key = concatenacao(nome);  //Calcula a chave usando o nome do contato
-    key = funHashDiv(key);  //Calcula o índice usando a função de hash
+    int key = funHashDiv(key);  //Calcula o índice usando a função de hash
     int key_inicio = key; // verificar se a função está entrando em um loop infinito ao procurar o contato na tabela.
 
     while(1){
@@ -90,8 +78,7 @@ struct contato *buscarContato(agendaDeContatos Hash, char *nome){
 
 //Função para remover um contato da tabela hash pelo nome
 void removerContato(agendaDeContatos Hash, char *nome){
-    int key = concatenacao(nome);  //Calcula a chave usando o nome do contato
-    key = funHashDiv(key); //Calcula o índice usando a função de hash
+   int key = funHashDiv(key); //Calcula o índice usando a função de hash
 
     int key_inicio = key;
 
